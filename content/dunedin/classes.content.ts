cc4fc3408b9f518491b734c4eafb936c090aebdf
Jason Liu <jasonliu215@gmail.com>
2023-09-03 22:41:06 +1200

    Updated content

--- content/dunedin/classes.content.ts
+++ content/dunedin/classes.content.ts
@@ -1,42 +1,62 @@
 export const allClasses: allClassInfo = {
   description: `
-    <p>
+      <p>
         <strong>European contredanses</strong><br/>
-        European contredanses, often simply called contredanses are a style of historical dance that originated in Europe during the 17th and 18th centuries.
-        English country dance and Scottish country dance are well known forms of contredanses. Contredanses typically involve groups of couples arranged in lines or squares and are characterized by a series of lively and coordinated group movements.  These dances played a significant in the evolution of social dance in Europe and transformed French dance.
+        European contredanses, often simply called contredanses are a style of historical dance that originated in Europe during the 17th and 18th centuries.<br/>
+        English country dance and Scottish country dance are well known forms of contredanses. <br/><br/>
+
+        Contredanses typically involve groups of couples arranged in lines or squares and are characterized by a series of lively and coordinated group movements.  These dances played a significant role in the evolution of social dance in Europe and transformed French dance.
         <br/><br/>
+
         <strong>Balfolk</strong><br/>
-        Balfolk dancing is a dynamic and inclusive genre of dance and music that draws from diverse European traditions. What sets it apart is its fusion of traditional elements with modern innovations and a willingness to adapt and evolve. Balfolk is characterized by its accessibility, emphasizing community, social interaction, experience and improvisation. Participants of all ages and skill levels come together to enjoy these dances.  This dance form encourages creativity and embraces change, making it a vibrant and ever-evolving experience.
+        Balfolk dancing is a dynamic and inclusive genre of dance and music that draws from diverse European traditions. <br/>
+        What sets it apart is its fusion of traditional elements with modern innovations and a willingness to adapt and evolve. <br/><br/>
+        Balfolk is characterized by its accessibility, emphasizing community, social interaction, experience and improvisation. <br/>
+        Participants of all ages and skill levels come together to enjoy these dances.  <br/>
+        This dance form encourages creativity and embraces change, making it a vibrant and ever-evolving experience.
         <br/><br/>
         <strong><em>Price:</em></strong> $40 for a block of 5 consecutive weeks - $10 casual attendance
-    </p>
+      <p/>
     `,
   allClasses: [
     {
       style: `English Country Dance <br/><strong>(Beginners)</strong>`,
       day: "Monday",
       time: "6:00pm - 7:00pm",
-      address: `Opoho Scout Hall <br/><br/>128 Opoho Road, <br/>Dunedin North`,
+      address: `
+        <strong>Opoho Scout Hall</strong> <br/><br/>
+        128 Opoho Road, <br/>
+        Dunedin North
+      `,
     },
     {
       style: "English Country Dance <br/><strong>(Beyond Basics)</strong>",
       day: "Monday",
       time: "7:00pm - 8:30pm",
-      address: `Opoho Scout Hall <br/><br/>128 Opoho Road, <br/>Dunedin North`,
+      address: `
+        <strong>Opoho Scout Hall </strong> <br/><br/>
+        128 Opoho Road, <br/>
+        Dunedin North
+      `,
     },
     {
       style: "Balfolk Immersion",
       day: "Tuesday",
       time: "7:00pm - 8:30pm",
-      address:
-        "Sacred Heart School Hall <br/>(Nga Maara Hall)<br/><br/>63 North Road, <br/>North East Valley",
+      address: `
+          <strong>Sacred Heart School Hall<br/>(Nga Maara Hall)</strong> <br/><br/>
+          63 North Road, <br/>
+          North East Valley
+        `,
     },
     {
       style: "Balfolk Squad <br/><strong>(by invitation)</strong>",
       day: "Wednesday",
       time: "7:00pm - 8:30pm",
-      address:
-        "North East Valley Baptist Community Centre <br/><br/> 270 North Road, <br/> North East Valley",
+      address: `
+        <strong>North East Valley Baptist Community Centre </strong><br/><br/>
+        270 North Road, <br/>
+        North East Valley`,
     },
   ],
   eventsLink: "https://www.eventfinda.co.nz/user/kategrace",
