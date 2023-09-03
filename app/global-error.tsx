f033cc35c29099e738436631931d0ad3458a8b33
Jason Liu <jasonliu215@gmail.com>
2023-09-03 23:14:02 +1200

    Update error page and press mention card

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,57 +1,79 @@
-'use client'
+"use client";
 
-import {PageWrapperAnimation} from "@/components/animations/common/PageWrapperAnimation";
-import styles from "@/app/global-error.module.scss";
+import { PageWrapperAnimation } from "@/components/animations/common/PageWrapperAnimation";
+import styles from "./global-error.module.scss";
 import Link from "next/link";
 
 export default function GlobalError({
-                                        error,
-                                        reset,
-                                    }: {
-    error: Error
-    reset: () => void
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
 }) {
-    console.log(error)
+  console.log(error);
 
-    return (
-        <html>
-        <body>
+  return (
+    <html>
+      <body>
         <PageWrapperAnimation>
-            <section className={`${styles.errorPage}`}>
-                <h1 className={`title`}> Something went wrong sorry :( </h1>
-                <section className={`flex flex-col justify-center items-center gap-5 mt-10`}>
-                    <section className={`flex flex-col justify-center items-center gap-1`}>
-                        <p>Please use the <strong>Home page</strong> below to go back to the <Link className={`link`}
-                                                                                                   href={"/"}>home
-                            page</Link>
-                        </p>
-                        <p>You can also use the <strong>Refresh</strong> button to reload the page</p>
-                    </section>
-                    <section className={`flex flex-row justify0center items-center gap-3`}>
-                        <button className={`${styles.button}`}>
-                            <Link className={`link`} href={"/"}>Home page</Link>
-                        </button>
-                        <button className={`${styles.button}`} onClick={() => reset()}>
-                            <p>Refresh Page</p>
-                        </button>
-                    </section>
-                    <section>
-                        <ul>
-                            <li>
-                                <p>If the issue continues, please contact us at
-                                    <a className={`link text-primary-dark-500`} href={`mailto:balfolkdunedni@gmail.com`}
-                                       target={"_blank"}> Balfolk Dunedin </a>
-                                    describing the issue.</p>
-                            </li>
-                            <li>
-                                <p>Please also include any screenshots if you can :)</p>
-                            </li>
-                        </ul>
-                    </section>
-                </section>
+          <section className={`${styles.errorPage}`}>
+            <h1 className={`title`}> Something went wrong sorry :( </h1>
+            <section
+              className={`flex flex-col justify-center items-center gap-5 mt-10`}
+            >
+              <section
+                className={`flex flex-col justify-center items-center gap-1`}
+              >
+                <p>
+                  Please use the <strong>Home page</strong> below to go back to
+                  the{" "}
+                  <Link className={`link`} href={"/"}>
+                    home page
+                  </Link>
+                </p>
+                <p>
+                  You can also use the <strong>Refresh</strong> button to reload
+                  the page
+                </p>
+              </section>
+              <section
+                className={`flex flex-row justify-center items-center gap-3`}
+              >
+                <button className={`${styles.button}`}>
+                  <Link className={``} href={"/"}>
+                    <p>Home page</p>
+                  </Link>
+                </button>
+                <button className={`${styles.button}`} onClick={() => reset()}>
+                  <p>Refresh Page</p>
+                </button>
+              </section>
+              <section>
+                <ul>
+                  <li>
+                    <p>
+                      If the issue continues, please contact us at
+                      <a
+                        className={`link text-primary-dark-500`}
+                        href={`mailto:balfolkdunedni@gmail.com`}
+                        target={"_blank"}
+                      >
+                        {" "}
+                        Balfolk Dunedin{" "}
+                      </a>
+                      describing the issue.
+                    </p>
+                  </li>
+                  <li>
+                    <p>Please also include any screenshots if you can :)</p>
+                  </li>
+                </ul>
+              </section>
             </section>
+          </section>
         </PageWrapperAnimation>
-        </body>
-        </html>
-    )
+      </body>
+    </html>
+  );
 }
--- components/newsroom/pressMentionCard/pressMentionCard.tsx
+++ components/newsroom/pressMentionCard/pressMentionCard.tsx
@@ -9,15 +9,20 @@ const PressMentionCard = ({
   link,
 }: pressMention) => {
   return (
-    <section
-      className={`${styles.card} w-full flex-1 | flex gap-3 flex-col justify-start items-start`}
-    >
-      <a className={``} href={`${link}`} target={"_blank"}>
+    <section className={`${styles.card} w-full flex-1`}>
+      <a
+        className={`flex flex-col justify-start items-start gap-6 md:gap-2`}
+        href={`${link}`}
+        target={"_blank"}
+      >
         <h3 className={`pb-3 ${styles.title}`}>{title}</h3>
-        <p>
-          By <strong>{author}</strong> - <span className="italic">{company}</span>
-        </p>
-        <p>On {date}</p>
+        <div>
+          <p>
+            By <strong>{author}</strong> -{" "}
+            <span className="italic">{company}</span>
+          </p>
+          <p>On {date}</p>
+        </div>
         <p className="pt-2">{shortDescription}</p>
       </a>
     </section>
