559950c450bc7d9aafb32559c7a2e8a9d3758c63
Jason Liu <jasonliu215@gmail.com>
2023-09-03 20:42:43 +1200

    Added wellington classes

--- content/wellington/classes.content.ts
+++ content/wellington/classes.content.ts
@@ -1,6 +1,22 @@
 export const allClasses: allClassInfo = {
-  description: `
+    description:
+    `
+    <article>
+    <h3 style="text-align: center; letter-spacing: 2px;">Coming soon!</h3>
+    <p style="margin-top: 1rem">
+    We're going to be starting another branch in Wellington very soon! <br>
+    If you want to know more, feel free to email our team at
+    <a className="activeLink" href="mailto:balfolkwellington@gmail.com">Balfolk Wellington </a> with any questions
+     that you might have</p>
+    </article>
     `,
-  allClasses: [],
-  eventsLink: "",
-};
+    allClasses: [
+        {
+            style: "Coming Soon!",
+            day: "Coming Soon!",
+            time: "Coming Soon!",
+            address: "Coming Soon"
+        },
+    ],
+    eventsLink: ""
+}
