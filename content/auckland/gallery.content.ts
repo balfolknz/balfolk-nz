c392a983fd3d9ab2324119e740158421c1ec7bf6
Jason Liu <jasonliu215@gmail.com>
2023-09-05 16:50:14 +1200

    Added auckland pictures and dunedin class

--- content/auckland/gallery.content.ts
+++ content/auckland/gallery.content.ts
@@ -1,3 +1,7 @@
 export const galleryInfo: galleryInfo = {
-    groupPhotoPaths: [],
-}
+  groupPhotoPaths: [
+    "/images/auckland/Balfolk Auckland Square 1.jpg",
+    "/images/auckland/Balfolk Auckland Square 2.jpg",
+    "/images/auckland/Balfolk Auckland Square 3.jpg",
+  ],
+};
