f86f606a81b80c8ee3472a1a0d25ef09a5e57ad3
Jason Liu <jasonliu215@gmail.com>
2023-09-05 17:01:17 +1200

    Updated picture paths for auckland

--- content/auckland/gallery.content.ts
+++ content/auckland/gallery.content.ts
@@ -1,7 +1,7 @@
 export const galleryInfo: galleryInfo = {
   groupPhotoPaths: [
     "/images/auckland/Balfolk Auckland Square 1.jpg",
-    "/images/auckland/Balfolk Auckland Square 2.jpg",
-    "/images/auckland/Balfolk Auckland Square 3.jpg",
+    "/images/auckland/Balfolk Auckland square 2.jpg",
+    "/images/auckland/Balfolk Auckland square 3.jpg",
   ],
 };
