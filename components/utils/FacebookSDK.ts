29912fd325f5c066926b1c0fbd942eca518ad35b
Jason Liu <jasonliu215@gmail.com>
2023-11-09 18:28:55 +1300

    Removed facebook utils

--- components/utils/FacebookSDK.ts
+++ components/utils/FacebookSDK.ts
@@ -1,31 +1,31 @@
-export const initFacebookSdk = () => {
-  return new Promise((resolve, reject) => {
-    // Load the Facebook SDK asynchronously
-    window.fbAsyncInit = () => {
-      window.FB.init({
-        appId: "1268250524120464",
-        cookie: true,
-        xfbml: true,
-        version: "v16.0",
-      });
-      // Resolve the promise when the SDK is loaded
-      resolve();
-    };
-  });
-};
+// export const initFacebookSdk = () => {
+//   return new Promise((resolve, reject) => {
+//     // Load the Facebook SDK asynchronously
+//     window.fbAsyncInit = () => {
+//       window.FB.init({
+//         appId: "1268250524120464",
+//         cookie: true,
+//         xfbml: true,
+//         version: "v16.0",
+//       });
+//       // Resolve the promise when the SDK is loaded
+//       resolve();
+//     };
+//   });
+// };
 
-export const getFacebookLoginStatus = () => {
-  return new Promise((resolve, reject) => {
-    window.FB.getLoginStatus((response) => {
-      resolve(response);
-    });
-  });
-};
+// export const getFacebookLoginStatus = () => {
+//   return new Promise((resolve, reject) => {
+//     window.FB.getLoginStatus((response) => {
+//       resolve(response);
+//     });
+//   });
+// };
 
-export const fbLogin = () => {
-  return new Promise((resolve, reject) => {
-    window.FB.login((response) => {
-      resolve(response);
-    });
-  });
-};
+// export const fbLogin = () => {
+//   return new Promise((resolve, reject) => {
+//     window.FB.login((response) => {
+//       resolve(response);
+//     });
+//   });
+// };
