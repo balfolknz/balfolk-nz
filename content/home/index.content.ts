4cff2cbca05d8e59cbec03ad8f8d183abea030a4
Jason Liu <jasonliu215@gmail.com>
2023-11-20 18:32:47 +1300

    Updated meet our team content

--- content/home/index.content.ts
+++ content/home/index.content.ts
@@ -37,7 +37,7 @@ export const allTeamContent: allTeamContent = {
       imagePath: "/images/profiles/michael parmenter/Michael Parmenter 2.jpg",
       description: `
       <p>
-        Michael Parmenter (Balfolk Auckland) has a long background in contemporary dance, having choreographed over 50 significant works for his own and other major companies. <br/>
+        Michael Parmenter <strong>(Balfolk Auckland)</strong> has a long background in contemporary dance, having choreographed over 50 significant works for his own and other major companies. <br/>
         He has developed the Partner Improvisation technique Piloting/TACTICS, has been dancing tango for 15 years and has recently been researching partner-dance history and promoting Balfolk dance in New Zealand. <br/><br/>
         He was the 2022 Caroline Plummer Fellow in Community Dance at the University of Otago.
       </p>`,
@@ -49,7 +49,7 @@ export const allTeamContent: allTeamContent = {
       imagePath: "/images/profiles/kate grace/Kate Grace 1.jpg",
       description: `
         <p>
-          Kate Grace (Balfolk Dunedin) hails originally from France where her early encounters with dance included African dance and Contact Improvisation. <br/>
+          Kate Grace <strong>(Balfolk Dunedin)</strong> hails originally from France where her early encounters with dance included African dance and Contact Improvisation. <br/>
           Upon moving to New Zealand in 1995 she taught African dance and drumming in Dunedin. <br/>
           During a three year visit to France from 2005 she reconnected with dancers and musicians interested in the folk revival and upon returning to Dunedin, she ran classes teaching traditional French and European dances as well as historical dances from various time periods.  <br/><br/>
 
