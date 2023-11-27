4432ae866ca912eff05010e45340f987abc172e1
Jason Liu <jasonliu215@gmail.com>
2023-11-27 23:30:27 +1300

    Updated classes

--- content/dunedin/classes.content.ts
+++ content/dunedin/classes.content.ts
@@ -1,57 +1,73 @@
 export const allClasses: allClassInfo = {
-      //   <p>
-      //   <strong>European contredanses</strong><br/>
-      //   European contredanses, often simply called contredanses are a style of historical dance that originated in Europe during the 17th and 18th centuries.<br/>
-      //   English country dance and Scottish country dance are well known forms of contredanses. <br/><br/>
+  // <p>
+  //   <strong>European contredanses</strong><br/>
+  //   European contredanses, often simply called contredanses are a style of historical dance that originated in Europe during the 17th and 18th centuries.<br/>
+  //   English country dance and Scottish country dance are well known forms of contredanses. <br/><br/>
 
-      //   Contredanses typically involve groups of couples arranged in lines or squares and are characterized by a series of lively and coordinated group movements.  These dances played a significant role in the evolution of social dance in Europe and transformed French dance.
-      //   <br/><br/>
+  //   Contredanses typically involve groups of couples arranged in lines or squares and are characterized by a series of lively and coordinated group movements.  These dances played a significant role in the evolution of social dance in Europe and transformed French dance.
+  //   <br/><br/>
 
-      //   <strong>Balfolk</strong><br/>
-      //   Balfolk dancing is a dynamic and inclusive genre of dance and music that draws from diverse European traditions. <br/>
-      //   What sets it apart is its fusion of traditional elements with modern innovations and a willingness to adapt and evolve. <br/><br/>
-      //   Balfolk is characterized by its accessibility, emphasizing community, social interaction, experience and improvisation. <br/>
-      //   Participants of all ages and skill levels come together to enjoy these dances.  <br/>
-      //   This dance form encourages creativity and embraces change, making it a vibrant and ever-evolving experience.
-      //   <br/><br/>
+  //   <strong>Balfolk</strong><br/>
+  //   Balfolk dancing is a dynamic and inclusive genre of dance and music that draws from diverse European traditions.
+  //   What sets it apart is its fusion of traditional elements with modern innovations and a willingness to adapt and evolve. <br/><br/>
+  //   Balfolk is characterized by its accessibility, emphasizing community, social interaction, experience and improvisation. <br/>
+  //   Participants of all ages and skill levels come together to enjoy these dances.  <br/>
+  //   This dance form encourages creativity and embraces change, making it a vibrant and ever-evolving experience.
+  //   <br/><br/>
 
-      //   <strong><em>Price:</em></strong>
-      //   $40 for a block of 5 consecutive weeks - $10 casual attendance <br/><br/>
+  //   If you have any questions, feel free to email our team at
+  //   <a className="activeLink" href="mailto:balfolkdunedin@gmail.com">Balfolk Dunedin!</a></p>
 
-      //   If you have any questions, feel free to email our team at
-      //   <a className="activeLink" href="mailto:balfolkdunedin@gmail.com">Balfolk Dunedin!</a></p>
-
-      // <p/>
+  // <p/>
   description: `
-      <p>
-        <strong>Balfolk</strong><br/>
-        Balfolk dancing is a dynamic and inclusive genre of dance and music that draws from diverse European traditions. <br/>
-        What sets it apart is its fusion of traditional elements with modern innovations and a willingness to adapt and evolve. <br/><br/>
-        Balfolk is characterized by its accessibility, emphasizing community, social interaction, experience and improvisation. <br/>
-        Participants of all ages and skill levels come together to enjoy these dances.  <br/>
-        This dance form encourages creativity and embraces change, making it a vibrant and ever-evolving experience.
-        <br/><br/>
+  <div>
+    <p>
+    <strong>European contredanses</strong><br/>
+    European contredanses, often simply called contredanses are a style of historical dance that originated in Europe during the 17th and 18th centuries.<br/>
+    English country dance and Scottish country dance are well known forms of contredanses. <br/><br/>
 
-        <strong><em>Price:</em></strong>
-        $40 for a block of 5 consecutive weeks - $10 casual attendance <br/><br/>
+    Contredanses typically involve groups of couples arranged in lines or squares and are characterized by a series of lively and coordinated group movements.  These dances played a significant role in the evolution of social dance in Europe and transformed French dance.
+    <br/><br/>
 
+    <strong>Balfolk</strong><br/>
+    Balfolk dancing is a dynamic and inclusive genre of dance and music that draws from diverse European traditions.
+    What sets it apart is its fusion of traditional elements with modern innovations and a willingness to adapt and evolve. <br/><br/>
+    </p>
+
+    <h3>Summer break</h1>
+    <p>
+      Classes have ended for 2023, but will resume on <strong>Wednesday 14th February 2024</strong>! <br/><br/>
+
+      Don't forget about our monthly minibals at <em>Toitū Otago Settlers Museum</em> on the last Sunday of each month.<br/>
+      Our next one will be on <strong>Sunday 25 February 2024</strong>
+    </p>
+
+    <br/><br/>
+
+    <p>
         If you have any questions, feel free to email our team at
         <a className="activeLink" href="mailto:balfolkdunedin@gmail.com">Balfolk Dunedin!</a></p>
-
-      <p/>
+    </p>
+    </div>
     `,
   allClasses: [
     {
-      style: `Balfolk Partner Dances <br/><strong>(Beginners)</strong>`,
-      day: "Monday",
-      time: "6:00pm - 7:00pm",
+      style: `European Contradances <br/>`,
+      day: "TBC",
+      time: "TBC",
       address: `
-        <strong>Opoho Scout Hall</strong> <br/><br/>
-
-        128 Opoho Road, <br/>
-        Dunedin North
+        TBC
       `,
-      pricePerWeek: "10",
+      pricePerWeek: "TBC",
+    },
+    {
+      style: `Balfolk Immersion <br/>`,
+      day: "TBC",
+      time: "TBC",
+      address: `
+        TBC
+      `,
+      pricePerWeek: "TBC",
     },
     // {
     //   style: "Balfolk Partner Dances <br/><strong>(Beyond Basics)</strong>",
@@ -65,28 +81,19 @@ export const allClasses: allClassInfo = {
     //   `,
     //   pricePerWeek: "10",
     // },
-    {
-      style: "Balfolk Immersion",
-      day: "Tuesday",
-      time: "7:00pm - 8:30pm",
-      address: `
-          <strong>Sacred Heart School Hall<br/>
-          (Nga Maara Hall)</strong> <br/><br/>
+    // {
+    //   style: "Balfolk Immersion",
+    //   day: "Tuesday",
+    //   time: "7:00pm - 8:30pm",
+    //   address: `
+    //       <strong>Sacred Heart School Hall<br/>
+    //       (Nga Maara Hall)</strong> <br/><br/>
 
-          63 North Road, <br/>
-          North East Valley
-        `,
-      pricePerWeek: "10",
-    },
-    {
-      style: "<strong>Event - </strong><em>Bal de la Saison Douce</em>",
-      day: "Saturday 25th November",
-      time: "<strong>TBC</strong>",
-      address: `
-          <strong>TBC</strong>
-        `,
-      pricePerWeek: "25",
-    },
+    //       63 North Road, <br/>
+    //       North East Valley
+    //     `,
+    //   pricePerWeek: "10",
+    // },
   ],
   eventsLink: "https://www.eventfinda.co.nz/user/kategrace",
 };
