6417968b7875310c202e801b7d94ab4eee8cbc77
Jason Liu <jasonliu215@gmail.com>
2024-03-17 00:28:43 +1100

    removed test data

--- content/dunedin/media.content.ts
+++ content/dunedin/media.content.ts
@@ -1,33 +1,33 @@
 export const allVideos: Array<videoInfo> = [
-  {
-    url: "https://www.youtube.com/embed",
-    title: "Dunedin Dance 1",
-  },
-  {
-    url: "https://www.youtube.com/embed",
-    title: "Dunedin Dance 1",
-  },
+  // {
+  //   url: "https://www.youtube.com/embed",
+  //   title: "Dunedin Dance 1",
+  // },
+  // {
+  //   url: "https://www.youtube.com/embed",
+  //   title: "Dunedin Dance 1",
+  // },
 ];
 
 export const allPlaylists: Array<playlistInfo> = [
-  {
-    url: "https://www.youtube.com/",
-    title: "Dunedin Playlist 1",
-    description: "Dunedin Description 1",
-    thumbnailPath: "/images/common/placeholder.svg",
-  },
-  {
-    url: "https://www.youtube.com/",
-    title: "Dunedin Playlist 1",
-    description: "Dunedin Description 1",
-    thumbnailPath: "/images/common/placeholder.svg",
-  },
-  {
-    url: "https://www.youtube.com/",
-    title: "Dunedin Playlist 1",
-    description: "Dunedin Description 1",
-    thumbnailPath: "/images/common/placeholder.svg",
-  },
+  // {
+  //   url: "https://www.youtube.com/",
+  //   title: "Dunedin Playlist 1",
+  //   description: "Dunedin Description 1",
+  //   thumbnailPath: "/images/common/placeholder.svg",
+  // },
+  // {
+  //   url: "https://www.youtube.com/",
+  //   title: "Dunedin Playlist 1",
+  //   description: "Dunedin Description 1",
+  //   thumbnailPath: "/images/common/placeholder.svg",
+  // },
+  // {
+  //   url: "https://www.youtube.com/",
+  //   title: "Dunedin Playlist 1",
+  //   description: "Dunedin Description 1",
+  //   thumbnailPath: "/images/common/placeholder.svg",
+  // },
 ];
 
 export const allMedia: mediaInfo = {
