eb17b76fa22c85f4e4b0205dc97b2754cd17fa3b
Jason Liu <jasonliu486@proton.me>
2025-04-19 11:45:45 +1000

    Touched up some styling

--- content/christchurch/classes.content.ts
+++ content/christchurch/classes.content.ts
@@ -6,18 +6,18 @@ export const allClasses: allClassInfo = {
         <h3>Le Bal du Voyage</h3>
         <p>
         Balfolk comes to Christchurch! <br/>
-        Dancing, live music and good company, this is an event you won't want to miss <br/>
+        Dancing, live music and good company, this is an event you won't want to miss!<br/>
         For further details, check out our <a href="https://www.facebook.com/events/1179218503662031/" className='link' target='_blank'>Facebook Event</a> <br/><br/>
         
-        <strong>Tickets<strong/>
+        <h4>Tickets</h4/>
         Tickets for sale at on <a href="https://www.eventfinda.co.nz/2025/le-bal-du-voyage/christchurch?fbclid=IwY2xjawJv29tleHRuA2FlbQIxMQABHhH2FAaWGORObLBWqRfCCeMWoDFiPZeC9SADn-MIH7sYUxj-gzU_jV-xtPzJ_aem_s_SeDnP3pnYpC4F41Lmfpw" className='link' target='_blank'>Eventfinda </a><br/>
         <br/><br/>
 
         <h4>Details</h4>
         <strong>Location: </strong>Irish Society Hall, 29 Domain Tce, Spreydon, Christchurch<br/>
         <strong>Time: </strong>Saturday 27th April, 7:00pm - 9:30pm<br/>
-        <strong>Facebook Event: </strong><a href="https://www.facebook.com/events/1179218503662031/" className='link' target='_blank'>Event</a> <br/>
-        <strong>Eventfinda Event: </strong><a href="https://www.eventfinda.co.nz/2025/le-bal-du-voyage/christchurch?fbclid=IwY2xjawJv29tleHRuA2FlbQIxMQABHhH2FAaWGORObLBWqRfCCeMWoDFiPZeC9SADn-MIH7sYUxj-gzU_jV-xtPzJ_aem_s_SeDnP3pnYpC4F41Lmfpw" className='link' target='_blank'>Event</a> <br/>
+        <strong>Facebook: </strong><a href="https://www.facebook.com/events/1179218503662031/" className='link' target='_blank'>Event</a> <br/>
+        <strong>Eventfinda: </strong><a href="https://www.eventfinda.co.nz/2025/le-bal-du-voyage/christchurch?fbclid=IwY2xjawJv29tleHRuA2FlbQIxMQABHhH2FAaWGORObLBWqRfCCeMWoDFiPZeC9SADn-MIH7sYUxj-gzU_jV-xtPzJ_aem_s_SeDnP3pnYpC4F41Lmfpw" className='link' target='_blank'>Event</a> <br/>
         </p>
         <br/><br/>
     </div>
