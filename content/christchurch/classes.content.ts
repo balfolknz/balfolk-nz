f6fa56fb5ee8c22643cdf7f5e86bf6f3b0c19e92
Jason Liu <jasonliu486@proton.me>
2025-04-29 17:35:16 +1000

    fix: update christchurch event

--- content/christchurch/classes.content.ts
+++ content/christchurch/classes.content.ts
@@ -3,22 +3,12 @@ export const allClasses: allClassInfo = {
     <div>
         <h2>Upcoming Events</h2><br/><br/>
 
-        <h3>Le Bal du Voyage</h3>
+        <h3>Join our mailing list</h3>
         <p>
-        Balfolk comes to Christchurch! <br/>
-        Dancing, live music and good company, this is an event you won't want to miss!<br/>
-        For further details, check out our <a href="https://www.facebook.com/events/1179218503662031/" className='link' target='_blank'>Facebook Event</a> <br/><br/>
-        
-        <h4>Tickets</h4/>
-        Tickets for sale at on <a href="https://www.eventfinda.co.nz/2025/le-bal-du-voyage/christchurch?fbclid=IwY2xjawJv29tleHRuA2FlbQIxMQABHhH2FAaWGORObLBWqRfCCeMWoDFiPZeC9SADn-MIH7sYUxj-gzU_jV-xtPzJ_aem_s_SeDnP3pnYpC4F41Lmfpw" className='link' target='_blank'>Eventfinda </a><br/>
-        <br/><br/>
+        After a successful Bal du Voyage, Kate Grace is planning to revisit Christchurch for another event! <br/>
+        Details are still to come, but it'll be filled with more dancing, live music and good company. <br/>
+        If you'd like to stay in the loop, please contact our team at <a className="activeLink" href="mailto:balfolkchristchurch@gmail.com">Balfolk Christchurch</a></p> and we can add you to our mailing list :)<br/>
 
-        <h4>Details</h4>
-        <strong>Location: </strong>Irish Society Hall, 29 Domain Tce, Spreydon, Christchurch<br/>
-        <strong>Time: </strong>Saturday 27th April, 7:00pm - 9:30pm<br/>
-        <strong>Facebook: </strong><a href="https://www.facebook.com/events/1179218503662031/" className='link' target='_blank'>Event</a> <br/>
-        <strong>Eventfinda: </strong><a href="https://www.eventfinda.co.nz/2025/le-bal-du-voyage/christchurch?fbclid=IwY2xjawJv29tleHRuA2FlbQIxMQABHhH2FAaWGORObLBWqRfCCeMWoDFiPZeC9SADn-MIH7sYUxj-gzU_jV-xtPzJ_aem_s_SeDnP3pnYpC4F41Lmfpw" className='link' target='_blank'>Event</a> <br/>
-        </p>
         <br/><br/>
     </div>
     `,
